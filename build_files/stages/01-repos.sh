e0be5f2ea894000effcf017d0ebde982a01a846a
KenzieMac130 <kenziemac130@protonmail.com>
2025-08-13 18:25:49 -0400

    Remove repeat repo adding due to pathing error

--- build_files/stages/01-repos.sh
+++ build_files/stages/01-repos.sh
@@ -10,4 +10,3 @@ systemctl disable flatpak-add-fedora-repos.service
 
 # VSCodium
 rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-dnf5 -y config-manager addrepo --from-repofile=/etc/yum.repos.d/vscodium.repo
