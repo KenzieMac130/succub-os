e28b20c8fc2011cdba86b590adac24b1acd4b47a
KenzieMac130 <kenziemac130@protonmail.com>
2025-08-13 18:18:41 -0400

    Manually add vscodium repo

--- build_files/stages/01-repos.sh
+++ build_files/stages/01-repos.sh
@@ -10,4 +10,4 @@ systemctl disable flatpak-add-fedora-repos.service
 
 # VSCodium
 rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-# yum repo already in etc...
+dnf5 -y config-manager addrepo --from-repofile=/etc/yum.repos.d/vscodium.repo
