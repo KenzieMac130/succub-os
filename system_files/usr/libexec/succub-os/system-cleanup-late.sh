7200195f8d2a17ab436d238186f74397ebf53029
Kenzie <kenziemac130@protonmail.com>
2025-08-18 13:58:29 -0400

    Update system-cleanup-late.sh
    Get rid of cleanup late

--- system_files/usr/libexec/succub-os/system-cleanup-late.sh
+++ system_files/usr/libexec/succub-os/system-cleanup-late.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # Remove Fedora Flatpaks
-flatpak remote-delete --system --force fedora || true
-flatpak remote-delete --user --force fedora || true
-flatpak remote-delete --system --force fedora-testing || true
-flatpak remote-delete --user --force fedora-testing || true
+#flatpak remote-delete --system --force fedora || true
+#flatpak remote-delete --user --force fedora || true
+#flatpak remote-delete --system --force fedora-testing || true
+#flatpak remote-delete --user --force fedora-testing || true
